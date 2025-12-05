a06aa37d69f1188ce9225581c3be67011fadcb77
Marcosgabrielpeixoto <mgpeixoto242@gmail.com>
2025-12-05 16:00:29 -0400

    Update DeliveryController.js

--- controllers/DeliveryController.js
+++ controllers/DeliveryController.js
@@ -1,1 +1,21 @@
+import SistemaEntrega from '../models/SistemaEntrega';
+import DeliveryService from '../services/DeliveryService';
+
+class DeliveryController {
+    constructor() {
+        this.sistema = new SistemaEntrega();
+        this.service = new DeliveryService(this.sistema);
+    }
+
+    async processNewDelivery(cliente, bairro, rua) {
+        try {
+            const { pedido, rota } = await this.service.novaEntrega(cliente, bairro, rua);
+            return { success: true, message: `Entregas registradas com sucesso. Pedido: #${pedido.seq}`, rota };
+        } catch (error) {
+            return { success: false, message: error.message };
+        }
+    }
+}
+
+export default DeliveryController;
 
