1fd78b2c744e1e8c5d4dc35a8700fd0fa7d4c484
Marcosgabrielpeixoto <mgpeixoto242@gmail.com>
2025-12-05 15:59:54 -0400

    Update OrderQueue.js

--- src/models/OrderQueue.js
+++ src/models/OrderQueue.js
@@ -1,1 +1,21 @@
+class OrderQueue {
+    constructor() {
+        this.items = []; 
+    }
+
+    insert(key, value) {
+        this.items.push({ key, value });
+    }
+
+    popMin() {
+        if (this.items.length === 0) return null;
+        return this.items.shift();
+    }
+
+    inorder() {
+        return this.items;
+    }
+}
+
+export default OrderQueue;
 
