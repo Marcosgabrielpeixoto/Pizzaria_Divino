c342305480b1f3b683210857f1f0e3570e266050
Marcosgabrielpeixoto <mgpeixoto242@gmail.com>
2025-12-08 16:04:22 -0400

    Update DeliveryService.js

--- serviços/DeliveryService.js
+++ serviços/DeliveryService.js
@@ -1,6 +1,17 @@
-class DeliveryService {
-    constructor(sistema) {
-        this.sistema = sistema;
+// services/DeliveryService.js
+import SistemaEntrega from '../models/SistemaEntrega.js';
+
+export default class DeliveryService {
+    constructor() {
+        this.sistema = new SistemaEntrega();
+    }
+
+    getAvailableBairros() {
+        return this.sistema.bairrosDisponiveis();
+    }
+
+    getRuasDoBairro(bairro) {
+        return this.sistema.ruasDoBairro(bairro);
     }
 
     novaEntrega(cliente, bairro, rua) {
@@ -10,7 +21,12 @@ class DeliveryService {
     entregarProxima() {
         return this.sistema.entregarProxima();
     }
+
+    rotaOtima() {
+        return this.sistema.rotaOtima();
+    }
+
+    getPendingOrders() {
+        return this.sistema.listarPendentes();
+    }
 }
-
-export default DeliveryService;
-
